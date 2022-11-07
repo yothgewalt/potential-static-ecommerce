ba3ed4f959b3dad7967a7f52c99e6da78522e384
Yongyuth Chuankhuntod <eine.schlacht@gmail.com>
2022-11-07 11:22:18 +0700

    tailwind: Added more configuration for ecommerce website.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,27 @@ module.exports = {
     ],
     theme: {
         extend: {},
+        screens: {
+            '0px': '0px',
+            '120px': '120px',
+            '280px': '280px',
+            '360px': '360px',
+            '375px': '375px',
+            '390px': '390px',
+            '414px': '414px',
+            '412px': '412px',
+            '540px': '540px',
+            '640px': '640px',
+            '768px': '768px',
+            '820px': '820px',
+            '912px': '912px',
+            '1024px': '1024px',
+            '1280px': '1280px',
+            '1536px': '1536px'
+        },
+        fontFamily: {
+            seeds: ['Seed Sans'],
+        },
     },
     plugins: [],
 }
